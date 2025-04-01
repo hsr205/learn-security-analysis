24abcc3710169660c541286d6ec309dfc295e6de
Henry Rothenberg <hsr205@aol.com>
2025-04-01 14:39:33 -0400

    Resolve vulnerability

--- pages/create_book.ts
+++ pages/create_book.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from 'express';
+import express, {Request, Response} from 'express';
 import Book from '../models/book';
-import express from 'express';
 import bodyParser from 'body-parser';
+import rateLimit from 'express-rate-limit';
 
 const router = express.Router();
 
@@ -10,27 +10,34 @@ const router = express.Router();
  * The function is called for every request to this router
  * It parses the body and makes it available under req.body
  */
-router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.urlencoded({extended: true}));
 router.use(express.json());
+
+const limiter = rateLimit({
+    windowMs: 5 * 1000, // 5 seconds
+    max: 1, // Limit each IP to 1 request per `windowMs`
+    message: 'Server is busy, please try again later.',
+});
+
 
 /**
  * @route POST /newbook
  * @returns a newly created book for an existing author and genre in the database
  * @returns 500 error if book creation failed
  */
-router.post('/', async (req: Request, res: Response) => {
-  const { familyName, firstName, genreName, bookTitle } = req.body;
-  if (familyName && firstName && genreName && bookTitle) {
-    try {
-      const book = new Book({});
-      const savedBook = await book.saveBookOfExistingAuthorAndGenre(familyName, firstName, genreName, bookTitle);
-      res.status(200).send(savedBook);
-    } catch (err: unknown) {
-      res.status(500).send('Error creating book: ' + (err as Error).message);
+router.post('/', limiter, async (req: Request, res: Response) => {
+    const {familyName, firstName, genreName, bookTitle} = req.body;
+    if (familyName && firstName && genreName && bookTitle) {
+        try {
+            const book = new Book({});
+            const savedBook = await book.saveBookOfExistingAuthorAndGenre(familyName, firstName, genreName, bookTitle);
+            res.status(200).send(savedBook);
+        } catch (err: unknown) {
+            res.status(500).send('Error creating book: ' + (err as Error).message);
+        }
+    } else {
+        res.send('Invalid Inputs');
     }
-  } else {
-    res.send('Invalid Inputs');
-  }
 });
 
 export default router;
